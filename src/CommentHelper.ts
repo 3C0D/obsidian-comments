bb436667f7103a4e8efc37eeec04b9e3d40c2be9
3C0D <gptfort@gmx.fr>
2024-04-16 19:51:32 +0200

    first push

---
+++ src/CommentHelper.ts
@@ -0,0 +1,197 @@
+import { Editor, MarkdownView } from "obsidian";
+import AdvancedComments from "./main";
+
+function commentType(codeBlockType: string) {
+	const cLikeTypes = [
+		"c",
+		"cpp",
+		"c++",
+		"clike",
+		"cs",
+		"go",
+		"java",
+		"js",
+		"javascript",
+		"ts",
+		"typescript",
+		"jsx",
+		"less",
+		"scss",
+		"jsonc",
+		"dataviewjs",
+		"empty",
+	];
+	const hashTypes = [
+		"python",
+		"py",
+		"ruby",
+		"bash",
+		"ps1",
+		"zsh",
+		"sh",
+		"applescript",
+		"yaml",
+		"yml",
+	];
+
+	const lua = ["lua", "sql"];
+	const html = ["html", "xml", "md"];
+	const css = ["css"];
+	const templater = ["templater"];
+	const bat = ["bat"];
+
+	const types = {
+		cLikeTypes: cLikeTypes,
+		hashTypes: hashTypes,
+		lua: lua,
+		html: html,
+		css: css,
+		templater: templater,
+		bat: bat,
+	};
+
+	for (const [name, values] of Object.entries(types)) {
+		if (values.includes(codeBlockType.toLowerCase())) {
+			return name;
+		}
+	}
+
+	return "unknown";
+}
+
+export function commentSelection(
+	editor: Editor,
+	selection: string,
+	codeBlockType: string | null,
+	blockComment: boolean
+) {
+	let commentedSelection = "";
+
+	if (codeBlockType) {
+		const varName = commentType(codeBlockType);
+		if (
+			(varName === "cLikeTypes" && !blockComment) ||
+			varName === "templater"
+		) {
+			const pattern = /^(\s*)\/\/\s?(.*)$/gm;
+			if (pattern.test(selection)) {
+				commentedSelection = selection.replace(pattern, `$1$2`);
+			} else {
+				commentedSelection = selection.replace(
+					/^(\s*)(.*)$/gm,
+					`$1// $2`
+				);
+			}
+		} else if (
+			(blockComment && varName === "cLikeTypes") ||
+			varName === "css"
+		) {
+			const pattern = /^\/\*\s?(.*)\s?\*\/$/gms;
+			if (pattern.test(selection)) {
+				commentedSelection = selection.replace(pattern, `$1`);
+			} else {
+				commentedSelection = selection.replace(/^(.*)$/gms, `/* $1 */`);
+			}
+		} else if (varName === "hashTypes") {
+			const pattern = /^(\s*)#\s?(.*)$/gm;
+			if (pattern.test(selection)) {
+				commentedSelection = selection.replace(pattern, `$1$2`);
+			} else {
+				commentedSelection = selection.replace(
+					/^(\s*)(.*)$/gm,
+					`$1# $2`
+				);
+			}
+		} else if (varName === "lua") {
+			const pattern = /^(\s*)--\s?(.*)$/gm;
+			if (pattern.test(selection)) {
+				commentedSelection = selection.replace(pattern, `$1$2`);
+			} else {
+				commentedSelection = selection.replace(
+					/^(\s*)(.*)$/gm,
+					`$1-- $2`
+				);
+			}
+		} else if (varName === "bat") {
+			const pattern = /^[Rr][Ee][Mm]\s?(.*)$/gm;
+			if (pattern.test(selection)) {
+				commentedSelection = selection.replace(pattern, `$1`);
+			} else {
+				commentedSelection = selection.replace(/^(.*)$/gm, `REM $1`);
+			}
+		} else if (varName === "html") {
+			const pattern = /^<!--\s?(.*)\s?-->$/gms;
+			if (pattern.test(selection)) {
+				commentedSelection = selection.replace(pattern, `$1`);
+			} else {
+				commentedSelection = selection.replace(
+					/^(.*)$/gms,
+					`<!-- $1  -->`
+				);
+			}
+		} else {
+			return;
+		}
+	} else {
+		const pattern = /^%%(.*)%%$/gms;
+		if (pattern.test(selection)) {
+			commentedSelection = selection.replace(pattern, `$1`);
+		} else {
+			commentedSelection = selection.replace(/^(.*)$/gms, `%% $1 %%`);
+		}
+	}
+
+	const { pi, pr } = getPosToOffset(editor, selection, blockComment);
+	const from = editor.offsetToPos(pi);
+	const to = editor.offsetToPos(pr);
+
+	editor.replaceRange(commentedSelection, from, to);
+}
+
+export function getPosToOffset(
+	editor: Editor,
+	sel: string,
+	blockComment: boolean
+) {
+	let i = editor.getCursor("from");
+	let r = editor.getCursor("to");
+	let pi = editor.posToOffset(i);
+	let pr = editor.posToOffset(r);
+	const value = editor.getLine(r.line);
+
+	if (!blockComment && pi === pr) {
+		i = { line: r.line, ch: 0 };
+		r = { line: r.line, ch: value.length };
+		pi = editor.posToOffset(i);
+		pr = editor.posToOffset(r);
+		sel = value;
+	} else if (
+		!blockComment &&
+		i.line !== r.line &&
+		(i.ch != 0 || r.ch != editor.getLine(r.line).length)
+	) {
+		const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		if (activeView) {
+			const data = this.app.metadataCache.getFileCache(
+				activeView.file
+			).sections;
+
+			if (
+				data.some(
+					(t: any) =>
+						t.type === "code" &&
+						t.position.start.line < i.line &&
+						t.position.end.line > r.line
+				)
+			) {
+				i = { line: i.line, ch: 0 };
+				r = { line: r.line, ch: value.length };
+				pi = editor.posToOffset(i);
+				pr = editor.posToOffset(r);
+				editor.setSelection(i, r);
+				sel = editor.getSelection();
+			}
+		}
+	}
+	return { pi, pr, sel };
+}
--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,107 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Plugin } from "obsidian";
+import { commentSelection, getPosToOffset } from "./CommentHelper";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class AdvancedComments extends Plugin {
 
 	async onload() {
-		console.log("totot")
-		await this.loadSettings();
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addCommand({
+			id: "advanced-comments",
+			name: "Line Comments",
+			editorCallback: (editor) => this.advancedComments(editor),
+		});
+		this.addCommand({
+			id: "advanced-blockComments",
+			name: "Block Comments",
+			editorCallback: (editor) => {
+				this.advancedComments(editor, true);
+			},
+		});
+
+		this.addCommand({
+			id: "trim-end-all-doc",
+			name: "Trim End All Doc",
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				const value = editor.getValue().replace(/[ \t]+$/gm, "");
+				editor.setValue(value);
+			},
+		});
+
+		this.addCommand({
+			id: "trim-end-code-blocks",
+			name: "Trim End Code Blocks",
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				const value = editor
+					.getValue()
+					.replace(
+						/^(```|~~~)([a-z0-9-+]+)\n([\s\S]*?)\n(?:```|~~~)$/gim,
+						(match,p0, p1, p2) => {
+							return (
+								p0 +
+								p1 +
+								"\n" +
+								p2.replace(/[ \t]+$/gm, "") +
+								"\n" + p0
+							);
+						}
+					);
+				editor.setValue(value);
+			},
+		});
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+	advancedComments = (editor: Editor, blockComment: boolean=false): void => {
+		// eslint-disable-next-line prefer-const
+		let { selection, value } = this.getSelectionAndValue(editor);
+		if (!blockComment && !selection) {
+			const curs = editor.getCursor();
+			const line = curs.line;
+			selection = editor.getLine(line);
+			if (selection.trim() === "") return;
+		}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+		const { pi, pr, sel } = getPosToOffset(editor, selection,blockComment);
+		const codeBlockType = this.getBlockType(editor, value, sel, pi, pr);
+		commentSelection(editor, sel, codeBlockType,blockComment);
+	};
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	getSelectionAndValue = (editor: Editor) => {
+		const selection = editor.getSelection();
+		const value = editor.getValue();
+		return { selection, value };
+	};
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+	getBlockType = (
+		editor: Editor,
+		value: string,
+		sel: string,
+		pi: number,
+		pr: number
+	): string | null => {
+		const codeBlockRegex = /^(?:```|~~~)([a-z0-9-+]*)\n([\s\S]*?)\n(?:```|~~~)$/gim; //case-insensitive
+		const templateBlockRegex = /^<%\*(.*?)%>$/gms;
+		const cursorIndex = Math.min(pi, pr);
+		let blockMatch;
 
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-	}
+		while ((blockMatch = codeBlockRegex.exec(value))) {
+			// find in what block selection is
+			if (
+				blockMatch.index <= cursorIndex &&
+				blockMatch.index + blockMatch[0].length >=
+					cursorIndex + sel.length
+			) {
+				return blockMatch[1] ? blockMatch[1] : "empty";
+			}
+		}
+		while ((blockMatch = templateBlockRegex.exec(value))) {
+			// find in what block selection is
+			if (
+				blockMatch.index <= cursorIndex &&
+				blockMatch.index + blockMatch[0].length >=
+					cursorIndex + sel.length
+			) {
+				return "templater";
+			}
+		}
+		return null;
+	};
 }
